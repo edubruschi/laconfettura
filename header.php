d9dc003d32f95a126c1e6a3118bd11c292a0cb97
Eduardo Bruschi Cardoso - Almap BBDO <eduardo.cardoso@md0011-6.almapbbdo.com.br>
2016-09-15 15:51:16 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?echo $title;?></title>
+  <link href="assets/bower_components/bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css"></link>
+  <link href="assets/css/app.css" rel="stylesheet" type="text/css"></link>
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$title="La Confettura";
+include 'header.php';
+?>
+<div class="img-container">
+  <div class="img img1"></div>
+  <div class="img img2"></div>
+  <div class="img img3"></div>
+  <div class="img img4">	  	
+	    <div class="link_email">
+	       <a href="mailto:laconfettura@laconfettura.com.br" target="_blank">&nbsp;</a>
+	    </div>
+	    <div class="link_page">
+	       <a href="http://www.laconfettura.com.br" target="_blank">&nbsp;</a>
+	    </div>
+  </div>
+  <div class="img img5">
+	    <div class="link_insta">
+	       <a href="https://www.instagram.com/laconfettura/" target="_blank">&nbsp;</a>
+	    </div>
+	    <div class="link_fb">
+	       <a href="https://www.facebook.com/laconfettura/" target="_blank">&nbsp;</a>
+	    </div>
+  </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
